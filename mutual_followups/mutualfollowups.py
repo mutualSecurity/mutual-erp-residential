c320eeed5557c036d981d2c540c262bc540b86af
parkash067 <pk_bscs@yahoo.com>
2016-10-20 14:38:03 +0500

    set riders technician and customers in project,followup and sales module

--- mutual_followups/mutualfollowups.py
+++ mutual_followups/mutualfollowups.py
@@ -1,19 +1,9 @@
 #The file name of this file must match the filename name which we import in __init__.py file
 from openerp.osv import fields, osv
 
-
-class rider_list(osv.osv):
-    _name="rider.list"
-    _columns = {
-        'name': fields.char('Name', size=64,required=True),
-        'rider_phone': fields.char('Mobile', size=11,required=True),
-    }
-
-
 class mutual_followups(osv.osv):
     _inherit = "res.partner"
     _columns = {
-        'riders': fields.many2one('rider.list', 'Rider', select=True, track_visibility='onchange'),
-    }
+        'riders': fields.many2one('res.partner', 'Assigned to Rider', required=False, select=1, track_visibility='onchange',domain="[('is_rider','=',True)]"),
 
-mutual_followups()
+    }
