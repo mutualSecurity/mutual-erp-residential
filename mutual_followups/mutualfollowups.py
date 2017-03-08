5f306149c8efab8076dd02b01541c839659187df
parkash067 <pk_bscs@yahoo.com>
2017-03-08 15:14:52 +0500

    add payment received check in followups

--- mutual_followups/mutualfollowups.py
+++ mutual_followups/mutualfollowups.py
@@ -4,6 +4,7 @@ from openerp.osv import fields, osv
 class mutual_followups(osv.osv):
     _inherit = "res.partner"
     _columns = {
+        'payment_received':fields.boolean('Payment Received',store=True,track_visibility='onchange'),
         'riders': fields.many2one('res.partner', 'Assigned to Rider', required=False, select=1, track_visibility='onchange',domain="[('is_rider','=',True)]"),
 
     }
