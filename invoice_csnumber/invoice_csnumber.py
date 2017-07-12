0547626e2b7c41f907773c32baa3a5ffedd125e5
parkash067 <pk_bscs@yahoo.com>
2017-07-12 13:18:11 +0500

    discount after tax on invoice

--- invoice_csnumber/invoice_csnumber.py
+++ invoice_csnumber/invoice_csnumber.py
@@ -19,7 +19,7 @@ class invoice_csnumber(osv.osv):
         'invoice_remarks': fields.char('Remarks',store=True),
         # 'cs_num': fields.char('CS Number', store=True, readonly=True, compute='cal_cs'),
         'css': fields.related('partner_id','cs_number',type='char', size=12,string='CS Number',readonly=True),
-        'css_number': fields.related('partner_id', 'cs_number', type='char', size=12,store=True, string='CS Number', readonly=True),
+        'css_number': fields.related('partner_id', 'cs_number', type='char', size=12, string='CS Number', readonly=True),
         'credit_card': fields.related('partner_id', 'credit_card_no', type='char',string='Credit Card', readonly=True),
         'outstanding': fields.related('partner_id', 'credit', type='char', string='Credit Balance', readonly=True),
         'phone': fields.related('partner_id','phone',type='char', size=12,string='Phone',readonly=True),
@@ -46,6 +46,14 @@ class invoice_csnumber(osv.osv):
         'cheque_no': fields.char('Cheque No.',store=True),
         'next_action': fields.date('Next Action,',store=True)
     }
+
+    @api.one
+    @api.depends('invoice_line.price_subtotal', 'tax_line.amount')
+    def _compute_amount(self):
+        self.amount_untaxed = sum(line.price_subtotal for line in self.invoice_line)
+        self.amount_tax = sum(line.amount for line in self.tax_line)
+        self.amount_total = self.amount_untaxed + self.amount_tax
+        self.amount_discount = sum(line.discount for line in self.invoice_line)
 
     @api.multi
     def invoice_validate(self):
@@ -207,3 +215,143 @@ class generalEntryCreate(osv.osv):
     _columns = {
         'parts_payment': fields.selection([('yes', 'Yes'), ('no', 'No')], 'Is this parts payment?',store=True, required=True),
     }
+
+
+class invoice_line_(osv.osv):
+    _inherit = 'account.invoice.line'
+    _columns = {
+        'discount_method': fields.selection([('fixed', 'Fixed'), ('percentage', 'Percentage')], 'Discount Method',store=True),
+    }
+
+    @api.one
+    @api.depends('price_unit', 'discount', 'invoice_line_tax_id', 'quantity',
+                 'product_id', 'invoice_id.partner_id', 'invoice_id.currency_id')
+    def _compute_price(self):
+        price = self.price_unit - self.discount
+        taxes = self.invoice_line_tax_id.compute_all(price, self.quantity, product=self.product_id,
+                                                     partner=self.invoice_id.partner_id)
+        self.price_subtotal = taxes['total']
+        if self.invoice_id:
+            self.price_subtotal = self.invoice_id.currency_id.round(self.price_subtotal)
+
+
+class mutual_account_invoice_tax(models.Model):
+    _inherit = "account.invoice.tax"
+
+    @api.one
+    @api.depends('base', 'base_amount', 'amount', 'tax_amount')
+    def _compute_factors(self):
+        self.factor_base = self.base_amount / self.base if self.base else 1.0
+        self.factor_tax = self.tax_amount / self.amount if self.amount else 1.0
+
+    @api.multi
+    def base_change(self, base, currency_id=False, company_id=False, date_invoice=False):
+        factor = self.factor_base if self else 1
+        company = self.env['res.company'].browse(company_id)
+        if currency_id and company.currency_id:
+            currency = self.env['res.currency'].browse(currency_id)
+            currency = currency.with_context(date=date_invoice or fields.Date.context_today(self))
+            base = currency.compute(base * factor, company.currency_id, round=False)
+        return {'value': {'base_amount': base}}
+
+    @api.multi
+    def amount_change(self, amount, currency_id=False, company_id=False, date_invoice=False):
+        company = self.env['res.company'].browse(company_id)
+        if currency_id and company.currency_id:
+            currency = self.env['res.currency'].browse(currency_id)
+            currency = currency.with_context(date=date_invoice or fields.Date.context_today(self))
+            amount = currency.compute(amount, company.currency_id, round=False)
+        tax_sign = (self.tax_amount / self.amount) if self.amount else 1
+        return {'value': {'tax_amount': amount * tax_sign}}
+
+    @api.v8
+    def compute(self, invoice):
+        tax_grouped = {}
+        currency = invoice.currency_id.with_context(date=invoice.date_invoice or fields.Date.context_today(invoice))
+        company_currency = invoice.company_id.currency_id
+        for line in invoice.invoice_line:
+            taxes = line.invoice_line_tax_id.compute_all(
+                (line.price_unit),
+                line.quantity, line.product_id, invoice.partner_id)['taxes']
+            print ">>>>>>>>>>>>>>>>>Taxes>>>>>>>>>>>>>>>>>>>>>>>"
+            print taxes
+            for tax in taxes:
+                val = {
+                    'invoice_id': invoice.id,
+                    'name': tax['name'],
+                    'amount': tax['amount'],
+                    'manual': False,
+                    'sequence': tax['sequence'],
+                    'base': currency.round(tax['price_unit'] * line['quantity']),
+                }
+                if invoice.type in ('out_invoice', 'in_invoice'):
+                    val['base_code_id'] = tax['base_code_id']
+                    val['tax_code_id'] = tax['tax_code_id']
+                    val['base_amount'] = currency.compute(val['base'] * tax['base_sign'], company_currency, round=False)
+                    val['tax_amount'] = currency.compute(val['amount'] * tax['tax_sign'], company_currency, round=False)
+                    val['account_id'] = tax['account_collected_id'] or line.account_id.id
+                    val['account_analytic_id'] = tax['account_analytic_collected_id']
+                else:
+                    val['base_code_id'] = tax['ref_base_code_id']
+                    val['tax_code_id'] = tax['ref_tax_code_id']
+                    val['base_amount'] = currency.compute(val['base'] * tax['ref_base_sign'], company_currency,
+                                                          round=False)
+                    val['tax_amount'] = currency.compute(val['amount'] * tax['ref_tax_sign'], company_currency,
+                                                         round=False)
+                    val['account_id'] = tax['account_paid_id'] or line.account_id.id
+                    val['account_analytic_id'] = tax['account_analytic_paid_id']
+
+                # If the taxes generate moves on the same financial account as the invoice line
+                # and no default analytic account is defined at the tax level, propagate the
+                # analytic account from the invoice line to the tax line. This is necessary
+                # in situations were (part of) the taxes cannot be reclaimed,
+                # to ensure the tax move is allocated to the proper analytic account.
+                if not val.get('account_analytic_id') and line.account_analytic_id and val[
+                    'account_id'] == line.account_id.id:
+                    val['account_analytic_id'] = line.account_analytic_id.id
+
+                key = (val['tax_code_id'], val['base_code_id'], val['account_id'])
+                if not key in tax_grouped:
+                    tax_grouped[key] = val
+                else:
+                    tax_grouped[key]['base'] += val['base']
+                    tax_grouped[key]['amount'] += val['amount']
+                    tax_grouped[key]['base_amount'] += val['base_amount']
+                    tax_grouped[key]['tax_amount'] += val['tax_amount']
+
+        for t in tax_grouped.values():
+            t['base'] = currency.round(t['base'])
+            t['amount'] = currency.round(t['amount'])
+            t['base_amount'] = currency.round(t['base_amount'])
+            t['tax_amount'] = currency.round(t['tax_amount'])
+
+        return tax_grouped
+
+    @api.v7
+    def compute(self, cr, uid, invoice_id, context=None):
+        recs = self.browse(cr, uid, [], context)
+        invoice = recs.env['account.invoice'].browse(invoice_id)
+        return mutual_account_invoice_tax.compute(recs, invoice)
+
+    @api.model
+    def move_line_get(self, invoice_id):
+        res = []
+        self._cr.execute(
+            'SELECT * FROM account_invoice_tax WHERE invoice_id = %s',
+            (invoice_id,)
+        )
+        for row in self._cr.dictfetchall():
+            if not (row['amount'] or row['tax_code_id'] or row['tax_amount']):
+                continue
+            res.append({
+                'type': 'tax',
+                'name': row['name'],
+                'price_unit': row['amount'],
+                'quantity': 1,
+                'price': row['amount'] or 0.0,
+                'account_id': row['account_id'],
+                'tax_code_id': row['tax_code_id'],
+                'tax_amount': row['tax_amount'],
+                'account_analytic_id': row['account_analytic_id'],
+            })
+        return res
