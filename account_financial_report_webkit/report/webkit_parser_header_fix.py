04e938a84a83f94898cfb07d7df5e8db3b6f76ab
parkash067 <pk_bscs@yahoo.com>
2017-10-12 11:24:52 +0500

    print trial balance in pdf form

--- account_financial_report_webkit/report/webkit_parser_header_fix.py
+++ account_financial_report_webkit/report/webkit_parser_header_fix.py
@@ -206,9 +206,8 @@ class HeaderFooterTextWebKitParser(webkit_report.WebKitParser):
         template = False
 
         if report_xml.report_file:
-            path = get_module_resource(
-                *report_xml.report_file.split(os.path.sep))
-            if os.path.exists(path):
+            path = get_module_resource(*report_xml.report_file.split('/'))
+            if path and os.path.exists(path):
                 template = file(path).read()
         if not template and report_xml.report_webkit_data:
             template = report_xml.report_webkit_data
