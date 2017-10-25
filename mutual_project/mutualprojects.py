d3656229a498a31e72249ddf4c4cc05df594828a
Ahsan Nasser <ahsnasser_23@hotmail.com>
2017-10-25 16:05:45 +0500

    project board improvement added

--- mutual_project/mutualprojects.py
+++ mutual_project/mutualprojects.py
@@ -15,6 +15,7 @@ class mutual_projects(osv.osv):
       'end_date': fields.date('End Date', store=True),
       'm_p': fields.selection([('Yes', 'Yes'), ('No', 'No')], 'Is monitoring period change?',
                                         store=True),
+      'recon_rem': fields.text('Reconnection Details', store=True),
       'system_status': fields.selection([('None','None'),('System Removed', 'System Removed')],'System Status', store=True),
       'description_remarks': fields.one2many('task.remarks', 'task_title', 'Description', store=True),
       'contract': fields.char('Contract', store=True, readonly=True),
