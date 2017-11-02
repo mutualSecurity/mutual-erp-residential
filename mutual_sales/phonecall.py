d4ef40e2b015d38a44c7cee79c890abb2dc7a514
parkash067 <pk_bscs@yahoo.com>
2017-11-02 10:14:16 -0700

    fix datetime issue in calllogs

--- mutual_sales/phonecall.py
+++ mutual_sales/phonecall.py
@@ -1,8 +1,8 @@
 from openerp import models, fields, api, _
-from datetime import timedelta
+from datetime import timedelta,datetime
 
 
 class mutualPhoneCall(models.Model):
     _inherit = 'crm.phonecall'  # Model identifer used for table name
 
-    date = fields.Datetime(string="Date", default=lambda self: fields.datetime.now()-timedelta(hours=5))
+    date = fields.Datetime(string="Date", default=lambda self: datetime.now()-timedelta(hours=5)-timedelta(hours=12),readonly=True)
