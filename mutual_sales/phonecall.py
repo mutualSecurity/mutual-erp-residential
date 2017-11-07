ad0609964ccf584b24ee2ed4518a055bff6f9778
hadi saeed <hadisaeed@outlook.com>
2017-11-07 17:06:58 +0500

    call logged time

--- mutual_sales/phonecall.py
+++ mutual_sales/phonecall.py
@@ -5,5 +5,5 @@ from datetime import timedelta,datetime
 class mutualPhoneCall(models.Model):
     _inherit = 'crm.phonecall'  # Model identifer used for table name
 
-    date = fields.Datetime(string="Date", default=lambda self: datetime.now()-timedelta(hours=5)-timedelta(hours=12),readonly=True)
+    date = fields.Datetime(string="Date", default=lambda self: datetime.now()-timedelta(hours=5),readonly=True)
     cs_number = fields.Char(related='partner_id.cs_number')
