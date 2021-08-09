eaaf945a642af3eec59ab23f8b904ad937323cdc
parkash067 <pk_bscs@yahoo.com>
2021-08-09 15:10:47 +0500

    add module

--- mutual_project/mutualprojects.py
+++ mutual_project/mutualprojects.py
@@ -32,6 +32,7 @@ class mutual_projects(osv.osv):
                                  domain="[('is_technician','=',True)]", default='', readonly=True),
       'finalstatus': fields.char("Final Status", store=True),
       'partner_id': fields.many2one('res.partner', 'Customer', required=True, domain="[('customer','=',True)]"),
+      'company_id': fields.related('partner_id', 'company_id', type='many2one', string='Company', readonly=True),
       'city_task': fields.related('partner_id', 'city', type='char', size=12, string='City', readonly=True),
       'monitoring_address_task': fields.related('partner_id', 'street', type='char', string='Monitoring address',readonly=True),
       'mobile_task': fields.related('partner_id', 'mobile', type='char', string='Mobile', readonly=True),
